7ecba9e5d43f9710f2dba94eb20da9f81827b2eb
meinayatraza <me.inayatraza@gmail.com>
2025-08-06 00:35:14 +0500

    Tailwind Installed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./aluminum_material/**/*.html",
+    "./apps/**/*.html",
+    "./templates/**/*.{html,js}",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
